b8598124a59a1c661500da43b99b9e694662f3c0
victorsiret <victorsiret@gmail.com>
2018-05-23 20:02:29 +0200

    js overview
    Added some JS to hide and show overview

--- dynamic.js
+++ dynamic.js
@@ -1,3 +1,21 @@
 [].forEach.call(document.querySelectorAll('.hideme'), function (el) {
     el.style.display = 'none';
   });
+  [].forEach.call(document.querySelectorAll('.hideme2'), function (el) {
+    el.style.display = 'none';
+  });
+
+//Show overview text
+var SO = 1;
+
+function showOverview() {
+  if (SO === 1) {
+  document.getElementsByClassName('hideme')[0].style.display = 'initial';
+  document.getElementsByClassName('overview')[0].textContent = '-';
+  SO = 0;}
+  else {
+  document.getElementsByClassName('hideme')[0].style.display = 'none';
+  document.getElementsByClassName('overview')[0].textContent = '+';
+  SO = 1;
+  }
+}
