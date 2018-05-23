9b83c6f4cb6f8550c4d047a227e828f5a8955428
victorsiret <victorsiret@gmail.com>
2018-05-23 23:10:39 +0200

    more js
    added collapsible contact menu

--- dynamic.js
+++ dynamic.js
@@ -5,6 +5,8 @@
     el.style.display = 'none';
   });
 
+
+//OVERVIEW
 //Show overview text
 var SO = 1;
 
@@ -19,3 +21,39 @@ function showOverview() {
   SO = 1;
   }
 }
+
+//Highlight overview
+
+function highlightOver() {
+  document.getElementsByClassName('plus')[0].style.color = 'tomato';
+}
+function unhighlightOver() {
+  document.getElementsByClassName('plus')[0].style.color = 'black';
+}
+
+//CONTACT
+//Show extra contact info
+
+var SC = 1;
+
+function showContact() {
+  if (SC === 1) {
+  document.getElementsByClassName('hideme')[1].style.display = 'initial';
+  document.getElementsByClassName('contact')[0].textContent = '-';
+  SC = 0;}
+  else {
+  document.getElementsByClassName('hideme')[1].style.display = 'none';
+  document.getElementsByClassName('contact')[0].textContent = '+';
+  SC = 1;
+  }
+}
+
+//Highlight contact
+
+function highlightContact() {
+  document.getElementsByClassName('plus')[1].style.color = 'tomato';
+}
+function unhighlightContact() {
+  document.getElementsByClassName('plus')[1].style.color = 'black';
+}
+
