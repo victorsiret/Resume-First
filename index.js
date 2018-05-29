e509b0712eea4e1ba1aba63c88fca410f51a4e66
victorsiret <victorsiret@gmail.com>
2018-05-29 19:16:19 +0200

    v
    v

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => res.send('Hello world'))
-app.get('/mysecret/', (req, res) => res.send('You are now on my secret page'))
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
