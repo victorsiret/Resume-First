e60e5c6f462e6ef9ed2415b749a71b3e6801a3db
Victor Siret <victorsiret@gmail.com>
2018-05-29 15:41:06 +0200

    g
    g

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-const http = require('http');
+const express = require('express')
+const app = express()
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app.get('/', (req, res) => res.send('Hello world'))
+app.get('/mysecret/', (req, res) => res.send('You are now on my secret page'))
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Students\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
